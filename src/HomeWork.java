d1fdaec481f72fce0b96da211c1a93b1e4c3ec2e
Samat <chegirovs@gmail.com>
2023-01-27 19:27:56 +0600

    add Homework class v2

---
+++ src/HomeWork.java
@@ -0,0 +1,14 @@
+public class HomeWork {
+    public static void main(String[] args) {
+
+        System.out.println(trueFalseMethod( 5, 2, 4, 6, 8, 3, 5, 7, 9));
+    }
+
+
+    static boolean trueFalseMethod(int a, int...varargs){
+
+
+        return true;
+    }
+
+}
